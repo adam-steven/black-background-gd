df7878cc030e9a6bb1d3d3111d7eb0ffe5b402ce
adam-steven <55250489+adam-steven@users.noreply.github.com>
2022-01-17 19:18:24 +0000

    Added camera shake functionality

--- script/GameController.cs
+++ script/GameController.cs
@@ -137,7 +137,7 @@ public class GameController : Node2D
 	#region Misc Functions 
 
 	//Starts turning the background red if player health is less than 30
-	public void UpdateBackgroundColor(int playerHealth) {
+	public void UpdateBackgroundColour(int playerHealth) {
 		//make sure the number is never less than 0
 		int red = Math.Max(0, 30 - playerHealth) * 2;
 		VisualServer.SetDefaultClearColor(Color.Color8((byte)red,0,0));
