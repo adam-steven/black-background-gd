bcf9ecaf50830e8a0294a7508b6d93a307e8e576
adam-steven <adam-steven@user.noreply.github.com>
2022-05-30 09:28:48 +0100

    Game over scene gets buttons

--- script/LevelControllers/DeathScreen.cs
+++ script/LevelControllers/DeathScreen.cs
@@ -4,7 +4,24 @@ using System;
 //DeathScreen.tscn 
 public class DeathScreen : Levels
 {
-    // public override void _Ready() {}
+	public override void _Ready() {
+		Godot.Control control = this.GetNode<Godot.Control>("Control");
+		Godot.VBoxContainer buttonContainer = control.GetNode<Godot.VBoxContainer>("Buttons");
+		Godot.Collections.Array buttons = buttonContainer.GetChildren();
 
-    // public override void LoadLevelParameters(System.Object sceneData) {}
+		for (int i = 0; i < buttons.Count; i++)
+		{
+			if(!buttons[i].GetType().Equals(typeof(GameOverButtons))) { continue; }
+		
+			Godot.Button button = (Godot.Button)buttons[i];
+			GD.Print(button.Name);
+			
+		}
+	}
+
+	//Handel score
+	public override void LoadLevelParameters(System.Object sceneData) {
+		if(sceneData == null) return;
+		GD.Print(((MainGameObj)sceneData).isQuickReset);
+	}
 }
