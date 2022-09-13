67b23503c95cf951b37c912d5813cfad5e364bd7
adam-steven <55250489+adam-steven@users.noreply.github.com>
2022-09-13 18:01:28 +0100

    Chaser attack tell

--- script/entities/enemies/fight/Chaser.cs
+++ script/entities/enemies/fight/Chaser.cs
@@ -1,9 +1,12 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 using static Enums;
 
 public class Chaser : Enemies
 {
+	private AnimationPlayer shootAnim;
+
 	public override void _Ready() 
 	{
 		FacePlayer();
@@ -12,14 +15,32 @@ public class Chaser : Enemies
 		Godot.Sprite thisSprite = this.GetNode<Godot.Sprite>("Sprite");
 		thisSprite.SelfModulate = colour;
 
+		shootAnim = this.GetNode<AnimationPlayer>("AnimationPlayer");
+		shootAnim.Connect("animation_finished", this, "Attack");
 		gun = new GunController(this); 
+
+		StartAttackTimer();
 	}
 
 	public override void _PhysicsProcess(float delta) 
 	{
 		TurnToPlayer(delta);
 		MoveInDirection(Vector2.Right);
+	}
 
-		gun.Shoot(BulletVariations.Normal);
+	private async void StartAttackTimer() {
+		int shotDelayMs = (int)(this.shotDelay * 1000) + 500; //+500 to account for anim time
+		await Task.Delay(shotDelayMs);
+
+		while(health > 0) {
+			shootAnim.Play("EnemyAttack");
+			await Task.Delay(shotDelayMs);
+		}
+	}
+
+	private void Attack(string animName = "") {
+		if(animName == "EnemyAttack") {
+			gun.Shoot(BulletVariations.Normal);
+		}
 	}
 }
