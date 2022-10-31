0e88c5c71a6a288519e3ac857bedd95ed0ed74ad
adam-steven <55250489+adam-steven@users.noreply.github.com>
2022-10-31 14:11:19 +0000

    qaud bullet obstacle's

--- script/entities/obstacles/types/MultiBulletSpawner.cs
+++ script/entities/obstacles/types/MultiBulletSpawner.cs
@@ -6,21 +6,21 @@ public class MultiBulletSpawner : Obstacle
 {
 	Random rnd = new Random();
 
-    [Export] bool facePlayer = true;
+	[Export] bool facePlayer = true;
 
-    [Export] List<string> spawnPoints = new List<string>();
-    List<GunController> guns = new List<GunController>();
-    
+	[Export] List<string> spawnPoints = new List<string>();
+	List<GunController> guns = new List<GunController>();
+	
 	internal override void _EntityReady()
 	{
 		SetProcess(facePlayer);
 
-        for (int i = 0; i < spawnPoints.Count; i++)
-        {
-            Node2D spawnPoint = this.GetNode<Node2D>(spawnPoints[i]);
-            GunController gun = new GunController(this, spawnPoint, GetTree());
-            guns.Add(gun);
-        }
+		for (int i = 0; i < spawnPoints.Count; i++)
+		{
+			Node2D spawnPoint = this.GetNode<Node2D>(spawnPoints[i]);
+			GunController gun = new GunController(this, spawnPoint, GetTree());
+			guns.Add(gun);
+		}
 
 		float spawnSpeedModifier = shotDelay / rnd.Next(1, 3);
 		anim.PlaybackSpeed = spawnSpeedModifier;
@@ -35,7 +35,7 @@ public class MultiBulletSpawner : Obstacle
 
 	private void ShootBullet(string animName)
 	{
-        for (int i = 0; i < guns.Count; i++) { guns[i].Shoot(); }
+		for (int i = 0; i < guns.Count; i++) { guns[i].Shoot(); }
 		EmitDeathSignal();
 	}
 }
